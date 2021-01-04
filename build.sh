aed3f81da9c716b01ff8552e1290e906ddb683dc
Brian Davies <brian@briandavies.me>
2021-01-03 18:10:25 -0700

    Add MongoDB to build

--- build.sh
+++ build.sh
@@ -114,6 +114,14 @@ sudo apt-get --yes install pgadmin4
 sudo -u postgres psql postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo -u postgres psql postgres -c "CREATE USER brian WITH SUPERUSER;"
 
+# MongoDB
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
 
 #######################################################################
 # Misc
