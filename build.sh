65cad1587f3d1222f4fa4b2d192eda1a6b98b4d6
Brian Davies <brian@briandavies.me>
2021-02-09 11:35:58 -0700

    Add Docker setup

--- build.sh
+++ build.sh
@@ -61,6 +61,31 @@ vim +'PlugInstall --sync' +qa
 
 
 #######################################################################
+# Docker
+#######################################################################
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
+#######################################################################
 # Apps
 #######################################################################
 sudo apt-get --yes install audacity chromium-browser evince flameshot gimp peek \
