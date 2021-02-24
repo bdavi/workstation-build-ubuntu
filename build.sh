0f758cf45256b254d7b3dcf04e8ba7513402fb03
Brian Davies <brian@briandavies.me>
2021-02-24 13:03:38 -0700

    Add postman

--- build.sh
+++ build.sh
@@ -126,6 +126,8 @@ sudo snap install --classic code
 
 sudo snap install datagrip --classic
 
+sudo snap install postman
+
 # Etcher
 echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
